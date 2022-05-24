58797170a2ddd148270c1dafb5bbd2a7c311121e
Francesco <83610036+donotlookhere@users.noreply.github.com>
2022-05-24 12:24:35 +0200

    Latex boilerplate adjustments

--- scripts/build:report.sh
+++ scripts/build:report.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mkdir -p ./build/docs/report;
-pdflatex -output-directory=./build/docs/report ./docs/report/report.tex
+lualatex -output-directory=./build/docs/report ./docs/report/report.tex
