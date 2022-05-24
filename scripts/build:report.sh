0c852d1d8d07e4157c7f4390537c1bb747219473
Matteo Bonacini <matteo.bonna@gmail.com>
2022-05-24 14:32:15 +0200

    Move build folder

--- scripts/build:report.sh
+++ scripts/build:report.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-mkdir -p ./docs/report/build;
-pdflatex -output-directory=./docs/report/build ./docs/report/report.tex
+mkdir -p ./build/docs/report;
+pdflatex -output-directory=./build/docs/report ./docs/report/report.tex
