b4802c56c98092d850e18e3b1911edd7eb03dda5
Matteo Bonacini <matteo.bonna@gmail.com>
2022-05-25 18:24:42 +0200

    Reword section 03

--- scripts/build:report.sh
+++ scripts/build:report.sh
@@ -3,4 +3,5 @@ mkdir -p ./build/docs/report &&
 export $(cat ./docs/report/.env | xargs) &&
 lualatex -output-directory=./build/docs/report ./docs/report/report.tex &&
 bibtex ./build/docs/report/report.aux &&
-lualatex -output-directory=./build/docs/report ./docs/report/report.tex; # Yep. Needed for bibtex.
+lualatex -output-directory=./build/docs/report ./docs/report/report.tex && # Yep. Needed for references.
+lualatex -output-directory=./build/docs/report ./docs/report/report.tex;   # Yep. Needed for bibtex.
