709389ed3096e765bf6172669b232e25057e14e9
Matteo Bonacini <matteo.bonna@gmail.com>
2022-05-25 12:43:04 +0200

    Add bibtex support

--- scripts/build:report.sh
+++ scripts/build:report.sh
@@ -2,3 +2,5 @@
 mkdir -p ./build/docs/report;
 export $(cat ./docs/report/.env | xargs);
 lualatex -output-directory=./build/docs/report ./docs/report/report.tex;
+bibtex ./build/docs/report/report.aux
+lualatex -output-directory=./build/docs/report ./docs/report/report.tex; # Yep. Needed for bibtex.
