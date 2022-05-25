fc330d9a90eb8de4a7238ffd8752e6bb60db55c4
Matteo Bonacini <matteo.bonna@gmail.com>
2022-05-25 15:25:46 +0200

    Split report into section Reword 01 and 02

--- scripts/build:report.sh
+++ scripts/build:report.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-mkdir -p ./build/docs/report;
-export $(cat ./docs/report/.env | xargs);
-lualatex -output-directory=./build/docs/report ./docs/report/report.tex;
-bibtex ./build/docs/report/report.aux
+mkdir -p ./build/docs/report &&
+export $(cat ./docs/report/.env | xargs) &&
+lualatex -output-directory=./build/docs/report ./docs/report/report.tex &&
+bibtex ./build/docs/report/report.aux &&
 lualatex -output-directory=./build/docs/report ./docs/report/report.tex; # Yep. Needed for bibtex.
