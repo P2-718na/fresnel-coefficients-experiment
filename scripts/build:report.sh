b116864aefa670265a7cc32dea092c3a5966ce64
Matteo Bonacini <matteo.bonna@gmail.com>
2022-05-31 18:46:41 +0200

    Feature/report (#2)
    Add lab report
    
    Co-authored-by: Francesco <83610036+donotlookhere@users.noreply.github.com>

--- scripts/build:report.sh
+++ scripts/build:report.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-mkdir -p ./build/docs/report;
-lualatex -output-directory=./build/docs/report ./docs/report/report.tex
+mkdir -p ./build/docs/report &&
+export $(cat ./docs/report/.env | xargs) &&
+lualatex -output-directory=./build/docs/report ./docs/report/report.tex &&
+bibtex ./build/docs/report/report.aux &&
+lualatex -output-directory=./build/docs/report ./docs/report/report.tex && # Yep. Needed for references.
+lualatex -output-directory=./build/docs/report ./docs/report/report.tex;   # Yep. Needed for bibtex.
